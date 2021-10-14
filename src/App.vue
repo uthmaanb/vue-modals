f9dfc07ae70e812f57b85fb2eb2843c4238bbeec
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2021-10-14 08:46:48 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">click me</button>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <!-- this is called a slot -->
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Ninja Giveaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </teleport>
+
+  <div v-if="showModal2">
+    <Modal theme="sale" @close="toggleModal2">
+      <!-- this is called a slot -->
+      <template v-slot:links>
+        <a href="#">sign up now2</a>
+        <a href="#">more info2</a>
+      </template>
+      <h1>Ninja Giveaway!2</h1>
+      <p>Grab your ninja swag for half price!2</p>
+    </Modal>
+  </div>
+  <button @click="toggleModal">open modal</button>
+  <button @click="toggleModal2">open modal2</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import modal
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "my first vue app",
+      showModal: false,
+      showModal2: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+#app,
+.modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
